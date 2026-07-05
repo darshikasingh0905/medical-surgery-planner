fa5e53c0f547d306177c0e073a53597cabbc1bd1
nish-09 <darshikasinghhh@gmail.com>
2026-07-06 00:42:45 +0530

    Implement ScanMetadata dataclass and metadata extraction pipeline

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 
 from src.loaders.nifti_loader import load_nifti
+from src.loaders.metadata import extract_metadata
 
 
 def main():
     scan_path = Path("datasets/raw/ct/ct_15mm_defaced.nii")
+
     image = load_nifti(scan_path)
 
-    print("CT scan loaded successfully!")
-    print(f"Image Type: {type(image)}")
-    print(f"Shape: {image.shape}")
+    metadata = extract_metadata(image)
+
+    print(metadata)
 
 
 if __name__ == "__main__":
--- src/loaders/metadata.py
+++ src/loaders/metadata.py
@@ -1,13 +1,8 @@
-"""
-metadata.py
-
-Utilities for extracting metadata from NIfTI medical images.
-"""
-
 from dataclasses import dataclass
 
 import nibabel as nib
 import numpy as np
+
 
 @dataclass
 class ScanMetadata:
@@ -15,7 +10,22 @@ class ScanMetadata:
     voxel_spacing: tuple
     data_type: str
     affine: np.ndarray
-    
+
+    def __repr__(self):
+        output = "========== CT Scan Metadata ==========\n"
+
+        output += f"Shape           : {self.shape}\n"
+        output += f"Voxel Spacing   : {self.voxel_spacing} mm\n"
+        output += f"Data Type       : {self.data_type}\n"
+
+        output += "\nAffine Matrix:\n"
+        output += str(self.affine)
+
+        output += "\n======================================"
+
+        return output
+
+
 def extract_metadata(image: nib.Nifti1Image) -> ScanMetadata:
     """
     Extract important metadata from a NIfTI image.
@@ -26,6 +36,15 @@ def extract_metadata(image: nib.Nifti1Image) -> ScanMetadata:
     Returns:
         ScanMetadata object containing essential scan information.
     """
-    
+
     shape = image.shape
-    
+    voxel_spacing = tuple(float(value) for value in image.header.get_zooms())    
+    data_type = image.header.get_data_dtype()
+    affine = image.affine
+
+    return ScanMetadata(
+        shape=shape,
+        voxel_spacing=voxel_spacing,
+        data_type=data_type,
+        affine=affine,
+    )
